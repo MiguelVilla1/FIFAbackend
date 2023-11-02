c8653a1c45c43dd6ede8c2c746d01580e8c92f14
MiguelVilla1 <mhvillah@icloud.com>
2023-11-01 21:00:43 -0700

    cors addition

--- api/integration.py
+++ api/integration.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify
-import random
+import random 
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 # Define position values
 CF = "Center Forward"
