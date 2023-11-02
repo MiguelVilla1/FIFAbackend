c9043c8a90679ff29ef06a8a8cde1a8f342f025c
aidandelgado2 <atdelgado12@gmail.com>
2023-11-01 22:36:19 -0700

    integration model fix aidan 11/2

--- model/integrationmodel.py
+++ model/integrationmodel.py
@@ -1,63 +1,66 @@
-from flask import Flask, jsonify, request
-import random
+from flask import Flask
 from flask_cors import CORS
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from flask_restful import Api, Resource, reqparse
+import random
 
 app = Flask(__name__)
 CORS(app)
+api = Api(app)
 
 # Define position values
-# ... (same as in your code)
+CF = "Center Forward"
+ST = "Striker"
+RB = "Right Back"
+LB = "Left Back"
+CM = "Central Midfield"
+LW = "Left Wing"
+CDM = "Central Defensive Midfield"
+CB = "Center Back"
+RM = "Right Midfield"
+LM = "Left Midfield"
+LD = "Left Back"
+CAM = "Central Attacking Midfield"
+RW = "Right Wing"
+LWB = "Left Wing Back"
+RWB = "Right Wing Back"
 
 class Player:
-    # ... (same as in your code)
+    def __init__(self, name, pos, ovr, pac, sho, pas, dri, defe, phy):
+        self.name = name
+        self.pos = pos
+        self.ovr = ovr
+        self.pac = pac
+        self.sho = sho
+        self.pas = pas
+        self.dri = dri
+        self.defe = defe
+        self.phy = phy
 
-    players = [
-    # ... (same as in your code)
+players = [
+    (Player("Lionel Messi", CF, 90, 80, 87, 90, 94, 33, 64), 1 / 100),
+    (Player("Cristiano Ronaldo", ST, 86, 85, 90, 75, 92, 45, 78), 1 / 86),
+    # Add more player data here...
 ]
 
 weights = [1 if player.ovr <= 70 else 0.5 for player, _ in players]
 
-# Create a list of features and labels for the ML model
-features = []
-labels = []
+class RandomPlayer(Resource):
+    def get(self):
+        random_player, _ = random.choices(players, weights=weights)[0]
+        player_info = {
+            "Name": random_player.name,
+            "Position": random_player.pos,
+            "Overall": random_player.ovr,
+            "Pace": random_player.pac,
+            "Shooting": random_player.sho,
+            "Passing": random_player.pas,
+            "Dribbling": random_player.dri,
+            "Defense": random_player.defe,
+            "Physicality": random_player.phy
+        }
+        return player_info
 
-for player, _ in players:
-    feature = [player.pac, player.sho, player.pas, player.dri, player.defe, player.phy]
-    label = player.pos
-    features.append(feature)
-    labels.append(label)
-
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
-
-# Create a Decision Tree classifier and fit it to the data
-clf = DecisionTreeClassifier()
-clf.fit(X_train, y_train)
-
-@app.route('/get_random_player', methods=["GET"])
-def get_random_player():
-    random_player, _ = random.choices(players, weights=weights)[0]
-    player_info = {
-        "Name": random_player.name,
-        "Position": random_player.pos,
-        "Overall": random_player.ovr,
-        "Pace": random_player.pac,
-        "Shooting": random_player.sho,
-        "Passing": random_player.pas,
-        "Dribbling": random_player.dri,
-        "Defense": random_player.defe,
-        "Physicality": random_player.phy
-    }
-
-    # Use the ML model to predict the player's position
-    features = [random_player.pac, random_player.sho, random_player.pas, random_player.dri, random_player.defe, random_player.phy]
-    predicted_position = clf.predict([features])[0]
-    player_info["Predicted Position"] = predicted_position
-
-    return jsonify(player_info)
+api.add_resource(RandomPlayer, '/get_random_player')
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8281)
